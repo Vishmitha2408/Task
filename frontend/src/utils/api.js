d88cb3114e23821ed12a6830368f938ec6d54059
Vishmitha2408 <vishuvishmitha23@gmail.com>
2025-05-25 23:53:49 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://task-back-fo2o.onrender.com',
+  baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
